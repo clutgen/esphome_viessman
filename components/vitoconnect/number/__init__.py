6835155cf17d48ea32b76ea512b0ff9a700a51da
Corentin Lutgen <corentin.lutgen@be.thalesgroup.com>
2023-12-19 18:49:07 +0100

    wip

--- components/vitoconnect/number/__init__.py
+++ components/vitoconnect/number/__init__.py
@@ -1,8 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import CONF_ID, CONF_NAME, CONF_ADDRESS, CONF_LENGTH #, CONF_TYPE 
-from .. import vitoconnect_ns, VitoConnect, CONF_VITOCONNECT_ID
+from esphome.const import CONF_ID, CONF_NAME, CONF_ADDRESS
+from .. import vitoconnect_ns, VitoConnect, CONF_VITOCONNECT_ID, CONF_MAX_VALUE, CONF_MIN_VALUE, CONF_STEP
 
 DEPENDENCIES = ["vitoconnect"]
 OPTOLINKNumber = vitoconnect_ns.class_("OPTOLINKNumber", number.Number)
@@ -10,7 +10,10 @@ OPTOLINKNumber = vitoconnect_ns.class_("OPTOLINKNumber", number.Number)
 CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(OPTOLINKNumber),
     cv.GenerateID(CONF_VITOCONNECT_ID): cv.use_id(VitoConnect),
-    cv.Required(CONF_ADDRESS): cv.uint16_t
+    cv.Required(CONF_ADDRESS): cv.uint16_t,
+    cv.Required(CONF_MAX_VALUE): cv.uint16_t,
+    cv.Required(CONF_MIN_VALUE): cv.uint16_t,
+    cv.Required(CONF_STEP): cv.uint16_t,
 })
 
 async def to_code(config):
