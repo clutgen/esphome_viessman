6c24a334643cab2fc0e2264b131e6bbd4050ffe0
Corentin Lutgen <corentin.lutgen@be.thalesgroup.com>
2023-12-19 19:17:58 +0100

    wip

--- components/vitoconnect/number/vitoconnect_number.cpp
+++ components/vitoconnect/number/vitoconnect_number.cpp
@@ -11,5 +11,23 @@ OPTOLINKNumber::~OPTOLINKNumber() {
   // empty
 }
 
+void OPTOLINKNumber::decode(uint8_t* data, uint8_t length, Datapoint* dp) {
+  assert(length >= _length);
+
+  if (!dp) dp = this;
+
+  publish_state(data[0]);
+}
+
+void OPTOLINKNumber::encode(uint8_t* raw, uint8_t length, void* data) {
+  float value = *reinterpret_cast<float*>(data);
+  encode(raw, length, value);
+}
+
+void OPTOLINKNumber::encode(uint8_t* raw, uint8_t length, float data) {
+  assert(length >= _length);
+
+}
+
 }  // namespace vitoconnect
 }  // namespace esphome
--- components/vitoconnect/number/vitoconnect_number.h
+++ components/vitoconnect/number/vitoconnect_number.h
@@ -6,11 +6,16 @@
 namespace esphome {
 namespace vitoconnect {
 
-class OPTOLINKNumber : public number::Number {
+class OPTOLINKNumber : public number::Number, public Datapoint {
 
   public:
     OPTOLINKNumber();
     ~OPTOLINKNumber();
+
+    void decode(uint8_t* data, uint8_t length, Datapoint* dp = nullptr) override;
+    void encode(uint8_t* raw, uint8_t length, void* data) override;
+    void encode(uint8_t* raw, uint8_t length, float data);
+
 };
 
 }  // namespace vitoconnect
