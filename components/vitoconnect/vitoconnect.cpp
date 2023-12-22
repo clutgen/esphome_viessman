f2c9f6ba77cc84ae5fe4693622ba7221ba80a5c6
Corentin Lutgen <corentin.lutgen@be.thalesgroup.com>
2023-12-22 17:08:15 +0100

    test

--- components/vitoconnect/vitoconnect.cpp
+++ components/vitoconnect/vitoconnect.cpp
@@ -73,6 +73,7 @@ void VitoConnect::update() {
       if (dp->getWrite()) 
       {
         ESP_LOGD(TAG, "getWrite true");
+        if (dp->getData() != nullptr) ESP_LOGD(TAG, "getWrite true value : %d", *(dp->getData()));
         if (dp->getData() != nullptr && _optolink->write(dp->getAddress(), dp->getLength(), dp->getData(), reinterpret_cast<void*>(arg))) {
         } else {
             delete arg;
